0d8ce269eee2c50dbd922ad28cffab9ff3ea4fee
Parv Khandelwal <parv10046@gmail.com>
2026-04-03 18:24:42 +0530

    Fixed Webhook Controller

--- src/main/java/net/parvkhandelwal/controller/WebHookController.java
+++ src/main/java/net/parvkhandelwal/controller/WebHookController.java
@@ -1,6 +1,7 @@
 package net.parvkhandelwal.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import net.parvkhandelwal.dto.ChatRequest;
 import net.parvkhandelwal.dto.ChatResponse;
 import net.parvkhandelwal.service.ChatBotService;
@@ -10,12 +11,23 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/webhook")
 @RequiredArgsConstructor
+@Slf4j
 public class WebHookController {
     private final ChatBotService chatBotService;
 
-    @GetMapping
-    public ResponseEntity<String>verifyWebhook(@RequestParam(value = "hub.challenge", required = false) String challenge) {
-        return ResponseEntity.ok(challenge != null ? challenge : "Webhook Server is UP. Waiting for Meta Handshake...");
+    @GetMapping("/webhook")
+    public ResponseEntity<String> verifyWebhook(
+            @RequestParam(value = "hub.mode", required = false) String mode,
+            @RequestParam(value = "hub.verify_token", required = false) String token,
+            @RequestParam(value = "hub.challenge", required = false) String challenge
+    ) {
+        String verifyToken = "test123";
+
+        if ("subscribe".equals(mode) && verifyToken.equals(token)) {
+            log.info("Webhook verified!");
+            return ResponseEntity.ok(challenge);
+        }
+        return ResponseEntity.status(403).body("Forbidden");
     }
     @PostMapping
     public ResponseEntity<ChatResponse> receiveMessage(@RequestBody ChatRequest chatRequest) {
